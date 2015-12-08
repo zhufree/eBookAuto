2954eeb6afcdaedd60c1fb18f1b1aba2f3d5a654
zhufree <zhufree2013@gmail.com>
2015-12-08 19:47:45 +0800

    add sound and video supprt

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 ﻿# 电子书自动排版说明文档
 
-标签（空格分隔）： 作业 出版
-
 ---
 
 ##功能
->自动读取文件夹中的文字，图片，（视频音频还未实现），生成html页面。
+>自动读取文件夹中的文字,图片,视频,音频,生成html页面。
 
 ##配置
 运行环境要求：python2.7+
@@ -23,11 +21,12 @@ Pillow==2.3.0
 - 内容文件放在特定文件夹中
  - **text**-文本文件
  - **pics**-图片文件
- - **videos**-视频文件（暂不支持）
+ - **videos**-视频文件
  - **sounds**-音频文件
  - **base.txt**-html-模板文件
 如下：
 ```
+.
 ├── base.txt
 ├── Charpter1.html
 ├── Charpter2.html
@@ -38,11 +37,19 @@ Pillow==2.3.0
 │   ├── pic2.jpg
 │   ├── pic3.jpg
 │   ├── pic4.jpg
-│   └── pic5.jpg
+│   ├── pic5.jpg
+│   ├── pic6.jpg
+│   └── small_pic6.jpg
+├── README.md
 ├── requirement.txt
+├── sounds
+│   └── sound1.mp3
 ├── text
-    ├── Charpter1.txt
-    └── Charpter2.txt
+│   ├── Charpter1.txt
+│   └── Charpter2.txt
+├── videos
+│   └── video1.mp4
+
 ```
 
-- 图片需要在文本内容中标注出来，并尽量和图片的文件名保持格式一致，如"pic1""pic2",或"img1","img2"，代码中可以修改文件名关键词。
+- 图片需要在文本内容中标注出来，并和图片的文件名保持格式一致，如"pic1""pic2",或"img1","img2"，代码中可以修改文件名关键词。
--- main.py
+++ main.py
@@ -1,55 +1,144 @@
 # coding: utf-8
 
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from PIL import Image
 
 import os
 import re
 
-# read html text files
+# read html template text files
 with open('base.txt', 'r+') as f:
     text = f.read()
 
 
-def insert_img(keyword, para, temp):
+def change_img_size(img, new_pic_url):
+    print '==========change img size=========='
+    row_width, row_height = img.size[0], img.size[1]
+    print 'row_size' + str(img.size)
+    radio = float(row_width)/300
+    to_width = 300
+    to_height = int(row_height/radio)
+    print 'change to ' + str((to_width, to_height))
+    img.thumbnail((to_width, to_height))
+    img.save(os.path.join('./pics', new_pic_url))
+    return img
+
+
+def insert_img(img_keyword, para, temp, count):
     """
-    :param keyword:word for search in text to show here should be a picture, such as 'img', 'pic', '图片'
-    :param para:one paragraphs in a charpter.
+    :param img_keyword:word for search in text to show here should be a picture, such as 'img', 'pic', '图片'
+    :param para:one paragraphs in a chapter.
+    :param temp: template of html
+    :param count: count for img at left or right side
     :return new_div: create a tag of the picture, to insert into html.
     """
-    if keyword in para:
-        pic_id = re.search(keyword + r'(\d+)', para).group()
+    if img_keyword in para:
+        # search pic id in current para, like 'pic1','img1'
+        pic_id = re.search(img_keyword + r'(\d+)', para).group()
+        print '==========insert img ' + pic_id + '=========='
+        # get path of the pic, like './pics/pic1'
         pic_url = [
             url for url in os.listdir('./pics') if url.startswith(pic_id)][0]
-        print pic_url
+        # use pillow lib to open the pic
         im = Image.open(os.path.join('./pics', pic_url))
-        print im.size  # (wedth,height)
-        if im.size[0] > 400:
+        # decide where to locate the pic
+        # rules: 1. if picture's width > 1/3 of the browser width
+        # and picture's width > height: locate it center
+        # 2. if picture's width > 1/3 of the browser width
+        # and picture's width < height:zoom the pic and locate it at side
+        # 3. if picture's width <1/3 of the browser width : locate it at side
+        # 4. when locate pictures at side ,put it at left first, then right.
+        if im.size[0] > 400 and im.size[0] > im.size[1]:
+            # create a div to put the img
             new_div = temp.new_tag('div')
+            # create a img tag
             new_pic = temp.new_tag('img', src='./pics/' + pic_url)
+            # add class to div
             new_div['class'] = 'pic_in_text_center'
+            # add img to div
             new_div.append(new_pic)
+        elif im.size[0] > 400 and im.size[0] < im.size[1]:
+            new_pic_url = 'small_' + pic_url
+            im = change_img_size(im, new_pic_url)
+            im.save(os.path.join('./pics', new_pic_url))
+            if count[0] > count[1]:
+                new_div = temp.new_tag('img', src='./pics/' + new_pic_url)
+                new_div['class'] = 'pic_in_text_right'
+                count[1] += 1
+            else:
+                new_div = temp.new_tag('img', src='./pics/' + new_pic_url)
+                new_div['class'] = 'pic_in_text_left'
+                count[0] += 1
         else:
-            if int(pic_id[-1]) % 2 == 0:
+            if count[0] > count[1]:
                 new_div = temp.new_tag('img', src='./pics/' + pic_url)
                 new_div['class'] = 'pic_in_text_right'
+                count[1] += 1
             else:
                 new_div = temp.new_tag('img', src='./pics/' + pic_url)
                 new_div['class'] = 'pic_in_text_left'
+                count[0] += 1
+        return new_div, count
+    else:
+        return None, count
+
+
+def insert_sound(sound_keyword, para, temp):
+    """
+    :param sound_keyword:word for search in text to show here should be a sound file, such as 'sound', 'music', '音乐'
+    :param para:one paragraphs in a chapter.
+    :param temp: template of html
+    :return new_div: create a tag of the picture, to insert into html.
+    """
+    if sound_keyword in para:
+        # search sound id in current para, like 'sound1','img1'
+        sound_id = re.search(sound_keyword + r'(\d+)', para).group()
+        print '==========insert sound ' + sound_id + '=========='
+        # get path of the sound, like './sounds/sound1'
+        sound_url = [
+            url for url in os.listdir('./sounds') if url.startswith(sound_id)][0]
+        new_div = temp.new_tag('audio', src='./sounds/' + sound_url, controls="controls")
+        new_div['class'] = 'sound_in_text'
         return new_div
     else:
         return None
 
 
+def insert_video(video_keyword, para, temp):
+    """
+    :param video_keyword:word for search in text to show here should be a video file, such as 'video', 'music', '音乐'
+    :param para:one paragraphs in a chapter.
+    :param temp: template of html
+    :return new_div: create a tag of the picture, to insert into html.
+    """
+    if video_keyword in para:
+        # search video id in current para, like 'video1','img1'
+        video_id = re.search(video_keyword + r'(\d+)', para).group()
+        print '==========insert video ' + video_id + '=========='
+        # get path of the video, like './videos/video1'
+        video_url = [
+            url for url in os.listdir('./videos') if url.startswith(video_id)][0]
+        new_div = temp.new_tag(
+        	'video', 
+        	src='./videos/' + video_url, 
+        	controls="controls", 
+        	width="600", 
+        	height="450"
+        	)
+        new_div['class'] = 'video_in_text'
+        return new_div
+    else:
+        return None
+
 def handle_text(filename):
     """
-    :param paras:file name of a charpter, such like 'Charpter1.txt',without directory path.
+    :param paras:file name of a charpter, such like 'Charpter1.txt', without directory path.
     :result: a html file
     """
+    # open file and read paragraphs
     with open(os.path.join('./text/', filename), 'r+') as f:
         paras = [p.strip() for p in f.readlines() if len(p) > 3]
-        # print paras
-
+    # read html template
     temp = BeautifulSoup(text, "lxml")
 
     # replace cover img
@@ -57,28 +146,38 @@ def handle_text(filename):
     cover['src'] = './pics/cover.jpg'
 
     # handle title
-    title = temp.find('h4')
+    title = temp.find('h3')
     title.string = paras[0]
 
     # handle paras
     textbox = temp.find('div', {'id': 'text'})
+    count = [0,0]
     for i in range(1, len(paras)):
         new_p = temp.new_tag('p')
         new_br = temp.new_tag('br')
         new_p.string = paras[i]
         # handle img in text
-        new_div = insert_img('pic', paras[i], temp)
-        if new_div:
-            textbox.append(new_div)
+        img_result = insert_img('pic', paras[i], temp, count)
+        new_img_div, count = img_result[0], img_result[1]
+        if new_img_div:
+            textbox.append(new_img_div)
+        new_sound_div = insert_sound('sound', paras[i], temp)
+        if new_sound_div:
+            textbox.append(new_sound_div)
+        new_video_div = insert_video('video', paras[i], temp)
+        if new_video_div:
+            textbox.append(new_video_div)
         textbox.append(new_p)
         textbox.append(new_br)
 
     with open(filename[:-4] + '.html', 'w+') as f:
         f.write(temp.prettify("utf-8"))
+        print '==========finish ' + filename + '==========' 
 
 
 if __name__ == '__main__':
     # find files that exsit in 'text' directory
-    files = os.listdir('./text')
+    files = [f for f in os.listdir('./text') if f.endswith('.txt')]
     for filename in files:
+        print '==========handle ' + filename + '=========='
         handle_text(filename)
